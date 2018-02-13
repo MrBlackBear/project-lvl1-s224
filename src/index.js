4dc66b64e146001b0ff9dab6a20d24f3093f2cb1
Anatoli Shalobasov <proboostbiznes@gmail.com>
2018-02-13 17:13:34 +0300

    third step

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import readlineSync from 'readline-sync';
 
-export const sayHello = () => {
-  console.log("Welcome to the Brain Games!\n");
-	const userName = readlineSync.question('May I have your name? ');
-	console.log(`Hello,${userName}!`);
+const sayHello = () => {
+  console.log('Welcome to the Brain Games!\n');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello,${userName}!`);
 };
+
+export default sayHello;
